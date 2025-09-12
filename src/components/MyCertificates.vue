940e0f22735abe3f3a4acc67b79425231b6e11cf
khalilSarme <ks8929801@gmail.com>
2025-09-12 23:27:34 +0300

    new script

--- src/components/MyCertificates.vue
+++ src/components/MyCertificates.vue
@@ -8,7 +8,7 @@
         <v-col cols="12" sm="6" md="4" lg="0" v-for="cer in cers" :key="cer">
           <v-card flat border>
             <v-card-text style="line-height: 0">
-              <img :src="`src/assets/${cer.src}`" alt="" class="w-100">
+              <img :src="`assets/${cer.src}`" alt="" class="w-100">
             </v-card-text>
           </v-card>
         </v-col>
